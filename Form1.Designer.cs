79f624353486917d87aeafb52f40b0e854bfb97e
parinyakon <68211219+parinyakon@users.noreply.github.com>
2020-08-27 19:11:28 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace oop_6115261021_w09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblBMI = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.taxCalculate = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.Label();
+            this.salary = new System.Windows.Forms.Label();
+            this.tax = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(290, 125);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "คำนวณ";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Location = new System.Drawing.Point(74, 51);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(42, 13);
+            this.lblWeight.TabIndex = 1;
+            this.lblWeight.Text = "น้ำหนัก";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(74, 92);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(38, 13);
+            this.lblHeight.TabIndex = 2;
+            this.lblHeight.Text = "ส่วนสูง";
+            // 
+            // lblBMI
+            // 
+            this.lblBMI.AutoSize = true;
+            this.lblBMI.Location = new System.Drawing.Point(74, 135);
+            this.lblBMI.Name = "lblBMI";
+            this.lblBMI.Size = new System.Drawing.Size(67, 13);
+            this.lblBMI.TabIndex = 3;
+            this.lblBMI.Text = "ดัชนีมวลกาย";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(140, 48);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(100, 20);
+            this.txtWeight.TabIndex = 4;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(140, 89);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 5;
+            // 
+            // taxCalculate
+            // 
+            this.taxCalculate.Location = new System.Drawing.Point(290, 275);
+            this.taxCalculate.Name = "taxCalculate";
+            this.taxCalculate.Size = new System.Drawing.Size(75, 23);
+            this.taxCalculate.TabIndex = 6;
+            this.taxCalculate.Text = "คำนวณภาษี";
+            this.taxCalculate.UseVisualStyleBackColor = true;
+            this.taxCalculate.Click += new System.EventHandler(this.taxCalculate_Click);
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(74, 201);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(20, 13);
+            this.name.TabIndex = 7;
+            this.name.Text = "ชื่อ";
+            // 
+            // salary
+            // 
+            this.salary.AutoSize = true;
+            this.salary.Location = new System.Drawing.Point(74, 242);
+            this.salary.Name = "salary";
+            this.salary.Size = new System.Drawing.Size(51, 13);
+            this.salary.TabIndex = 8;
+            this.salary.Text = "เงินเดือน";
+            // 
+            // tax
+            // 
+            this.tax.AutoSize = true;
+            this.tax.Location = new System.Drawing.Point(74, 285);
+            this.tax.Name = "tax";
+            this.tax.Size = new System.Drawing.Size(27, 13);
+            this.tax.TabIndex = 9;
+            this.tax.Text = "ภาษี";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(140, 198);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 10;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(140, 239);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(100, 20);
+            this.txtSalary.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.tax);
+            this.Controls.Add(this.salary);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.taxCalculate);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.lblBMI);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblBMI;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button taxCalculate;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label salary;
+        private System.Windows.Forms.Label tax;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSalary;
+    }
+}
+
